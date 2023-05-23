119935248ca98662d90897dfe83e507abfbaef95
whyuu18 <abdullahwahyudin18@gmail.com>
2023-05-23 16:52:44 +0800

    First upload final-project

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Abdullah Wahyudin'; // TODO: replace this
+
+const studentId = 'FE5588233'; // TODO: replace this
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
